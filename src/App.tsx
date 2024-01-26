4ceb955c800f240bfdcbf1fa39a9e97e43b2b129
agrawal-mahak <agrawalmahak7@gmail.com>
2024-01-26 18:23:08 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import {Navbar} from "./scene/navbar/index";
+import {SelectedPage} from './shared/types';
+import Home from "./scene/Home";
+import Benifits from "./scene/benifits";
+import {OurClasses} from "./scene/ourClasses"
+import {ContactUs} from "./scene/contactUs/index"
+import {Footer} from "./scene/footer/index";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>( 
+    SelectedPage.Home
   );
+  const [isTopOfPage, setIsTopPage] = useState<boolean>(true);
+
+  useEffect(() => {
+   const handleScroll = () => {
+    if(window.scrollY === 0) {
+      setIsTopPage(true);
+      setSelectedPage(SelectedPage.Home);
+    }
+    if(window.scrollY !== 0) setIsTopPage(false)
+   }
+   window.addEventListener("scroll", handleScroll);
+   return () => window.removeEventListener("scroll", handleScroll)
+  }, [])
+
+  return (
+    <div className="bg-gray-20">
+      <Navbar isTopOfPage = {isTopOfPage}
+         selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+      <Home setSelectedPage={setSelectedPage}/>
+      <Benifits setSelectedPage={setSelectedPage}/>
+      <OurClasses setSelectedPage={setSelectedPage}/>
+      <ContactUs setSelectedPage={setSelectedPage}/>
+      <Footer/>
+    </div>
+  );  
 }
 
 export default App;
